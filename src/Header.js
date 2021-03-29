00d5388e4028ca1c16015cf3c21eb27357da0bf2
doberman7 <ivanrubioangeles@gmail.com>
2021-03-28 19:29:10 -0600

    style

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Avatar, Image } from "antd";
+import { Avatar } from "antd";
 
 export const Header = () => {
   let today = new Date(),
@@ -12,8 +12,9 @@ export const Header = () => {
 
   return (
     <header>
-      <span className="date">{date}</span>
-      <h1>Ivan's</h1>
+      {/* <span className="date">{date}</span> */}
+      <p className="hello">Hi</p>
+      <p className="hello">This are my</p>
       <div className="avatar">
         <Avatar src="https://avatars.githubusercontent.com/u/23062808?s=400&u=8e2dab83bfa2b89a342788eb162a08dd9609c9c5&v=4" />
       </div>
