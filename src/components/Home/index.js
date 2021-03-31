0b01571a4e8fa6353c90a51dc07f594c93eeb953
doberman7 <ivanrubioangeles@gmail.com>
2021-03-30 21:18:42 -0600

    folders

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,12 @@
 import React from "react";
 
-import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
-import { Item } from "./Item";
-import { List } from "./List";
 import { Header } from "./Header";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ProjectLinks from "./ProjectLinks";
 import Contact from "./Contact";
 import Text from "./Text";
 import Purple from "./Purple";
-function Store({ match }) {
-  let { id } = match.params;
-  const imageHasLoaded = true;
-
-  return (
-    <>
-      <List selectedId={id} />
-      <AnimatePresence>
-        {id && imageHasLoaded && <Item id={id} key="item" />}
-      </AnimatePresence>
-    </>
-  );
-}
+import Rectangles from "./Rectangles";
 
 const Home = () => {
   return (
@@ -31,14 +15,8 @@ const Home = () => {
       <Purple />
 
       <Text />
-      <div className="container">
-        <AnimateSharedLayout type="crossfade">
-          <Router>
-            <Route path={["/:id", "/"]} component={Store} />
-          </Router>
-        </AnimateSharedLayout>
-      </div>
 
+      <Rectangles />
       <ProjectLinks />
       <Contact />
     </>
